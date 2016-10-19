a6541a1cff2e21aa880c6bce5db4e0b9903890d5
Carlos Espinal <cespinalt@gmail.com>
2016-10-19 09:45:45 -0700

    Use props to render the view properties

--- client/components/Restaurant.jsx
+++ client/components/Restaurant.jsx
@@ -4,9 +4,11 @@ import Stars from './Stars.jsx';
 const Restaurant = props => {
   return(
     <div className="col-md-4">
+      {console.log(props.restaurant)}
       <div className="well">
+        <h4 className="text-center">{props.restaurant.name}</h4>
         <img className="thumbnail img-responsive"
-          src="../assets/images/1.jpg" />
+          src={`../assets/images/${props.restaurant.id}.jpg`} />
         <h3 className="choice">Choose a rating</h3>
         <Stars />
       </div>
