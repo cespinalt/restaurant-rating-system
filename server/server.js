886c18db5059c742ee8f6cf5887852e7aaa8b5f5
Carlos Espinal <cespinalt@gmail.com>
2016-10-18 22:20:54 -0700

    Add routes configuration

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const configExpress = require('./config/express');
+const configRoutes = require('./routes');
+const {db} = require('./db');
 
 configExpress(app);
+configRoutes(app);
 
-app.listen(app.get('port'), () => {
-  console.log(`Server listening on port ${app.get('port')} in ${app.get('env')}\
-  mode...`);
-})
+db.sync()
+  .then(() => {
+    app.listen(app.get('port'), () => {
+      console.log(`Server listening on port ${app.get('port')} in ${app.get('env')}\
+      mode...`);
+    })
+  }).catch(err => console.log(err));
