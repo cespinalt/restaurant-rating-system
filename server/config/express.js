3ba58a9e27d9e855a35568ffcbdec580969697a5
Carlos Espinal <cespinalt@gmail.com>
2016-10-18 22:20:12 -0700

    Add line to set clientPath on the app

--- server/config/express.js
+++ server/config/express.js
@@ -8,11 +8,12 @@ const config = require('./environment');
 module.exports = app => {
   app.set('ip', config.ip);
   app.set('port', config.port);
+  app.set('clientPath', config.root);
   app.set('clientPath', `${app.get('clientPath')}/client`);
+  app.use(express.static(app.get('clientPath')));
   app.use(morgan('dev'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended: true}));
-  app.use(express.static(app.get('clientPath')));
 
   if(app.get('env') === 'development' || app.get('test') === 'test') {
     app.use(require('errorhandler')());
