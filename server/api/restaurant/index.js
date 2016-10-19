87ffd482b4b90c942ace71c39acb97075434e204
Carlos Espinal <cespinalt@gmail.com>
2016-10-19 00:09:55 -0700

    Add file to associate routes with controllers

---
+++ server/api/restaurant/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('./restaurant.controller');
+
+router.get('/', controller.all);
+router.post('/rate/', controller.rate);
+
+module.exports = router
