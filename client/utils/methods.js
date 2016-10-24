cb0909d2505f5622ca1d8a280e4b02cbc8bdd59e
Carlos Espinal <cespinalt@gmail.com>
2016-10-24 15:27:29 -0700

    Create file where all app methods are hosted

--- client/utils/methods.js
+++ client/utils/methods.js
@@ -1,20 +1,24 @@
 import axios from 'axios';
 
 const api = {}
+const uri = 'http://localhost:3000/api';
 
 api.getRestaurants = () => {
   // let restaurants;
-  return axios.get('http://localhost:3000/api');
+  return axios.get(uri);
 };
 
 api.submitRating = (userID, restID, value) => {
-  axios.post('/api/rate', {
-    params: { userID, restID, value,}
-  })
-  .then(() =>{
-
-  })
-  .catch()
-}
+  console.log(this);
+  // axios.post('/api/rate', {
+  //   params: { userID, restID, value,}
+  // })
+  // .then(() => {
+  //   // Do nothing
+  // })
+  // .catch(err => {
+  //   // Handle the err
+  // })
+};
 
 export default api;
