b282e290f30f17ad1499b9c8ac43e936e8a8d01e
Carlos Espinal <cespinalt@gmail.com>
2016-10-24 15:29:17 -0700

    Change columns on restaurant model

--- server/api/restaurant/restaurant.model.js
+++ server/api/restaurant/restaurant.model.js
@@ -1,9 +1,21 @@
 module.exports = (sequelize, DataTypes) => {
   return sequelize.define('restaurant', {
     name: DataTypes.STRING,
-    count_user: DataTypes.INTEGER,
-    reviews_sum: DataTypes.INTEGER,
+    users: {
+      type: DataTypes.INTEGER,
+      validate: {isNumeric: true,},
+    },
+    points: {
+      type: DataTypes.INTEGER,
+      validate: { isNumeric: true,},
+    }
   },{
+    getterMethods: {
+      average: function() {
+        const average = this.points / this.users;
+        return isNaN(average)  ? 0 : average;
+      }
+    },
     timestamps: false,
   });
 };
