377e6372aad7faf7074a084151d88a0f433eb276
Carlos Espinal <cespinalt@gmail.com>
2016-10-25 13:51:39 -0700

    Refactor call to submit rating

--- client/utils/methods.js
+++ client/utils/methods.js
@@ -4,21 +4,11 @@ const api = {}
 const uri = 'http://localhost:3000/api/restaurants';
 
 api.getRestaurants = () => {
-  // let restaurants;
   return axios.get(uri);
 };
 
-api.submitRating = (userID, restID, value) => {
-  console.log(this);
-  // axios.post('/api/rate', {
-  //   params: { userID, restID, value,}
-  // })
-  // .then(() => {
-  //   // Do nothing
-  // })
-  // .catch(err => {
-  //   // Handle the err
-  // })
+api.submitRating = (username, restID, value) => {
+  return axios.post('/api/restaurants/rate', {username, restID, value,})
 };
 
 export default api;
