54969eb3106209c8fbd6134bb117ef04f74c84bc
Carlos Espinal <cespinalt@gmail.com>
2016-10-24 23:14:42 -0700

    No changes to this file

--- server/api/user/user.model.js
+++ server/api/user/user.model.js
@@ -2,7 +2,12 @@ module.exports = (sequelize, DataTypes) => {
   return sequelize.define('user', {
     name: DataTypes.STRING,
     lastname: DataTypes.STRING,
-    username: DataTypes.STRING,
+    username: {
+      type: DataTypes.STRING,
+      unique: {
+        msg: 'The specified username is already in use.',
+      }
+    },
     password: DataTypes.STRING,
     email: {
       type: DataTypes.STRING,
