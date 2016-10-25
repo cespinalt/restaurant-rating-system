6b6f75c21d63ab4caaaf400b9cd77d898793fa93
Carlos Espinal <cespinalt@gmail.com>
2016-10-24 23:12:33 -0700

    Add routes to file

--- server/routes.js
+++ server/routes.js
@@ -3,14 +3,14 @@ const userRouter = require('./api/user');
 
 module.exports = app => {
   // app.use('/', restaurantRouter);
-  app.use('/api', restaurantRouter);
+  app.use('/api/restaurants', restaurantRouter);
   app.use('/api/user', userRouter);
 
   app.route('/:url(api|bower_components|assests|app)/*').get((req, res) => {
     res.sendStatus(404);
   });
 
-  app.route('/*').get((req, res) => {
+  app.route('/').get((req, res) => {
     res.sendFile(`${app.get('clientPath')}/index.html`);
   });
 };
