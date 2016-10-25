4c6392d8a6041d6729d020d3328bd766e69d14ba
Carlos Espinal <cespinalt@gmail.com>
2016-10-24 23:10:27 -0700

    Add routes for user, restaurant and non existing files

--- server/api/restaurant/index.js
+++ server/api/restaurant/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const controller = require('./restaurant.controller');
 
 router.get('/', controller.all);
+router.post('/', controller.add);
+router.delete('/', controller.delete);
 router.post('/rate/', controller.rate);
 
 module.exports = router
