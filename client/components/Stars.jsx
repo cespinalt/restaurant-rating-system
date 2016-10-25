5684a8c3977c9c5bd0fc6c8a1d9c77d4eb7c848b
Carlos Espinal <cespinalt@gmail.com>
2016-10-25 13:50:57 -0700

    Refactor the stars and add an onclick method to submit rating

--- client/components/Stars.jsx
+++ client/components/Stars.jsx
@@ -6,15 +6,27 @@ class Stars extends Component {
     super(props);
   }
 
+  onClickHandler(e) {
+    e.preventDefault();
+    const username = window.prompt('type your username')
+    const restID = this.props.id;
+    const value = e.target.value;
+    this.props.submitRating(username, restID, value);
+  }
   render() {
     return(
       <div>
         <span className="star-rating">
-          <input type="radio" name="rating" value="5" onClick={api.submitRating} /><i></i>
-          <input type="radio" name="rating" value="5" onClick={api.submitRating} /><i></i>
-          <input type="radio" name="rating" value="5" onClick={api.submitRating} /><i></i>
-          <input type="radio" name="rating" value="5" onClick={api.submitRating} /><i></i>
-          <input type="radio" name="rating" value="5" onClick={api.submitRating} /><i></i>
+          <input type="radio" name="rating" value="1"
+            onClick={this.onClickHandler.bind(this)} /><i></i>
+          <input type="radio" name="rating" value="2"
+            onClick={this.onClickHandler.bind(this)} /><i></i>
+          <input type="radio" name="rating" value="3"
+            onClick={this.onClickHandler.bind(this)} /><i></i>
+          <input type="radio" name="rating" value="4"
+            onClick={this.onClickHandler.bind(this)} /><i></i>
+          <input type="radio" name="rating" value="5"
+            onClick={this.onClickHandler.bind(this)} /><i></i>
         </span>
       </div>
     )
