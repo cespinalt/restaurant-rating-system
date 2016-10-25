b3e008dd2d50575c5e7e74b056d5b8b616ea8ad9
Carlos Espinal <cespinalt@gmail.com>
2016-10-25 13:49:56 -0700

    Change method that is passed through props to submit rating

--- client/components/Restaurant.jsx
+++ client/components/Restaurant.jsx
@@ -4,13 +4,15 @@ import Stars from './Stars.jsx';
 const Restaurant = props => {
   return(
     <div className="col-md-4">
-      {console.log(props.restaurant)}
       <div className="well">
         <h4 className="text-center">{props.restaurant.name}</h4>
         <img className="thumbnail img-responsive"
             src={`../assets/images/${props.restaurant.thumbnail}`} />
+          <span className="text-center">Rating: {props.restaurant.average} ||
+          Users Voted: {props.restaurant.users}</span>
         <h3 className="choice">Choose a rating</h3>
-        <Stars {...props.restaurant} />
+        <Stars {...props.restaurant}
+          submitRating={props.submitRating} />
       </div>
     </div>
   )
