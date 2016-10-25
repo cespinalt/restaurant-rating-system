c5b0e350c5f0e7b46cf76264449b3880f9e611c8
Carlos Espinal <cespinalt@gmail.com>
2016-10-24 23:12:00 -0700

    No changes to this file

--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -3,10 +3,33 @@ const {User} = require('../../db');
 const controller = {};
 
 controller.create = (req, res) => {
-  User.create({name: 'Pedro', lastname: 'John',
-  email: 'pjhon@gmail.com', password: '124'})
-    .then(results => res.json({msg: 'User created'}))
-    .catch(err => console.log(err));
+  const name = req.body.name;
+  const lastname = req.body.lastname;
+  const username = req.body.username;
+  const password = req.body.password;
+  const email = req.body.email;
+
+  User.findOrCreate({where: {name, lastname, password, username, email,}})
+  .spread((rest, created) => {
+    if(created) {
+      res.status(201).json({msg: 'username successfully added'});
+    } else {
+      res.status(409).json({msg: 'choose another username/email already exist'});
+    }
+  });
+};
+
+controller.remove = (req, res) => {
+  const username = req.body.username;
+
+  User.destroy({where: {username,}})
+  .then(data => {
+    if(data) {
+      res.status(202).json({msg: 'User deleted'});
+    } else {
+      res.status(202).json({msg: 'User not found'});
+    }
+  }).catch(err => { console.log(err)});
 };
 
 module.exports = controller;
